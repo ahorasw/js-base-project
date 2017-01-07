fda2b654f375fccba68d0aac0fcb0f7997d13dd4
Adriano Horas <ahoras@gmail.com>
2017-01-07 18:29:49 -0200

    Data Mocking chapter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import numeral from 'numeral'
-import './index.css'
-
+import {getUsers, deleteUser} from './api/userApi.js'
 /* eslint-disable no-console*/
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`Vou pagar ${courseValue} por esse curso!`);// eslint-disable-line no-console
+
+getUsers().then(result => {
+
+  let usersBody = "";
+
+  result.forEach(user => {
+
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`
+  });
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+  console.log(deleteLinks);
+  Array.from(deleteLinks, link =>{
+    link.onclick = function(event){
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
